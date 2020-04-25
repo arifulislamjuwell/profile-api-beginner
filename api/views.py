e0c93b34884b439ce7bff791db5cbb08d3167596
juwel <juwelariful1@gmail.com>
2020-04-25 14:58:13 +0600

    add post,put,patch and delete function to our API View

--- api/views.py
+++ api/views.py
@@ -1,7 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from .serializers import HelloSerializer
 
 class HelloApiView(APIView):
+
+    serializer_class= HelloSerializer
 
     def get(self, request, format= None):
 
@@ -10,3 +14,23 @@ class HelloApiView(APIView):
             'i think it should be work'
         ]
         return Response({'message': 'api_test', 'api_test':api_view})
+
+    def post(self, request):
+
+        serializer= self.serializer_class(data= request.data)
+
+        if serializer.is_valid():
+            name= serializer.validated_data.get('name')
+            message= 'hello {}'.format(name)
+            return Response ({'message': message})
+        else:
+            return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+    def put(self,request, pk=None):
+        return Response({'response': 'PUT'})
+    
+    def patch(self, request, pk=None):
+        return Response({'response': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        return Response({'response': 'DELETE'})
