65f1f4748550786683bad310c6b04ddff3167d54
juwel <juwelariful1@gmail.com>
2020-04-25 13:45:59 +0600

    create our first get api with APIView

--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+
+    def get(self, request, format= None):
+
+        api_view=[
+            'api testing',
+            'i think it should be work'
+        ]
+        return Response({'message': 'api_test', 'api_test':api_view})
