81ec2fa51c443b9b5d6ab373e362891d811f336e
juwel <juwelariful1@gmail.com>
2020-04-27 17:31:31 +0600

    added viewset view and route url

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,13 @@
 
-from django.urls import path
-from .views import HelloApiView
+from django.urls import path, include
+from .views import HelloApiView, HelloViewSets
+from rest_framework.routers import DefaultRouter
+
+router= DefaultRouter()
+router.register('hello-viewset', HelloViewSets, basename= "hello_viewsets")
 
 urlpatterns = [
-    path('hello-api/', HelloApiView.as_view())
+    path('hello-api/', HelloApiView.as_view()),
+    path('', include(router.urls))
+
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from rest_framework.views import APIView
+from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import HelloSerializer
@@ -24,7 +25,7 @@ class HelloApiView(APIView):
             message= 'hello {}'.format(name)
             return Response ({'message': message})
         else:
-            return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def put(self,request, pk=None):
         return Response({'response': 'PUT'})
@@ -34,3 +35,38 @@ class HelloApiView(APIView):
 
     def delete(self, request, pk=None):
         return Response({'response': 'DELETE'})
+
+    
+class HelloViewSets(ViewSet):
+    serializer_class= HelloSerializer
+    def list(self, request):
+        api_view=[
+            'api testing',
+            'i think it should be work'
+        ]
+        return Response({'message': 'api_test', 'api_test':api_view})
+
+    def create(self,request):
+        serializer= self.serializer_class(data= request.data)
+
+        if serializer.is_valid():
+            name= serializer.validated_data.get('name')
+            message= f'Hellow {name}'
+            return Response ({'message': message})
+        else:
+            return Response(
+                serializer.error,
+                status= status.HTTP_400_BAD_REQUEST
+            )
+    
+    def retrieve(self, request, pk):
+        return Response({'http': 'GET'})
+
+    def update(self, request, pk):
+        return Response({'http': 'PUT'})
+
+    def partial_update(self, request, pk):
+        return Response({'http': 'PATCH'})
+
+    def destroy(self, request, pk):
+        return Response({'http': 'DELETE'})
